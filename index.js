89fdbffd87cc1a00ed4cac650dd587621c3ef60d
Tuukka Hastrup <Tuukka.Hastrup@iki.fi>
2019-04-30 19:31:27 +0300

    WIP: Fetch facilities via a proxy

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var osmtogeojson = require('osmtogeojson');
 
 const api = "http://localhost:8080";
 
-const overpassQuery = "https://overpass-api.de/api/interpreter?data=%2F*%0AThis%20has%20been%20generated%20by%20the%20overpass-turbo%20wizard.%0AThe%20original%20search%20was%3A%0A%E2%80%9Cpark_ride%3D*%20in%20estonia%E2%80%9D%0A*%2F%0A%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3B%0A%2F%2F%20fetch%20area%20%E2%80%9Cestonia%E2%80%9D%20to%20search%20in%0Aarea%283600079510%29-%3E.searchArea%3B%0A%2F%2F%20gather%20results%0A%28%0A%20%20%2F%2F%20query%20part%20for%3A%20%E2%80%9Cpark_ride%3D*%E2%80%9D%0A%20%20node%5B%22park_ride%22%5D%28area.searchArea%29%3B%0A%20%20way%5B%22park_ride%22%5D%28area.searchArea%29%3B%0A%20%20relation%5B%22park_ride%22%5D%28area.searchArea%29%3B%0A%29%3B%0A%2F%2F%20print%20results%0Aout%20body%3B%0A%3E%3B%0Aout%20skel%20qt%3B";
+const overpassQuery = "https://estonia-parkandride--tuukkah.repl.co/";
 
 const getTileIndex = (url, callback) => {
     request({
@@ -34,30 +34,31 @@ class GeoJSONSource {
                 return;
             }
             this.facilityTileIndex = facilityTileIndex;
+            /* We don't have hubs for now:
             getTileIndex(overpassQuery, (err, hubTileIndex) => {
                 if (err) {
                     callback(err);
                     return;
                 }
-                this.hubTileIndex = hubTileIndex;
+                this.hubTileIndex = hubTileIndex;*/
                 callback(null, this);
-            })
+            //})
         })
     };
 
     getTile(z, x, y, callback) {
         let facilityTile = this.facilityTileIndex.getTile(z, x, y)
-        let hubTile = this.hubTileIndex.getTile(z, x, y)
+        // let hubTile = this.hubTileIndex.getTile(z, x, y)
 
         if (facilityTile === null) {
             facilityTile = {features: []}
         }
-
+/*
         if (hubTile === null) {
             hubTile = {features: []}
         }
-
-        const data = Buffer.from(vtPbf.fromGeojsonVt({facilities: facilityTile, hubs: hubTile}));
+*/
+        const data = Buffer.from(vtPbf.fromGeojsonVt({facilities: facilityTile, /*hubs: hubTile*/}));
 
         zlib.gzip(data, function (err, buffer) {
             if (err) {
@@ -82,7 +83,7 @@ class GeoJSONSource {
                 }],
             maxzoom: 20,
             minzoom: 1,
-            name: "HSL Park & Ride"
+            name: "Estonia Park & Ride"
         })
     }
 }
